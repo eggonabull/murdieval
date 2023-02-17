ad25d45a3ede1db91f38804aae0cf0e4e40d3350
Drew Faubion <eggonabull@hotmail.com>
2023-02-17 01:01:16 -0800

    Initial sprite loading

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,49 @@
 console.log('Try npm run lint/fix!');
 
-const longString =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+let bg: HTMLCanvasElement;
+let fg: HTMLCanvasElement;
 
-const trailing = 'Semicolon';
+var images_needed = 0;
+var images_loaded = 0;
+var images: {[key: string]: HTMLImageElement} = {};
 
-const why = 'am I tabbed?';
-
-export function doSomeStuff(
-  withThis: string,
-  andThat: string,
-  andThose: string[]
-) {
-  //function on one line
-  if (!andThose.length) {
-    return false;
+function image_loaded(this: GlobalEventHandlers, ev: Event) {
+  images_loaded += 1;
+  if (ev.target instanceof HTMLImageElement) {
+    if (ev.target.dataset.name) {
+      images[ev.target.dataset.name] = ev.target;
+      if (images_loaded == images_needed) {
+        begin();
+      }
+    } else {
+      console.log("bad name", ev.target.dataset.name)
+    }
+  } else {
+    console.log("bad image_loaded event type", typeof ev.target)
   }
-  console.log(withThis);
-  console.log(andThat);
-  console.dir(andThose);
-  return;
 }
-// TODO: more examples
+
+function loadImage(name: string, src: string) {
+  images_needed += 1;
+  var downloadingImage = new Image();
+  downloadingImage.dataset.name = name;
+  downloadingImage.onload = image_loaded;
+  downloadingImage.src = src
+}
+
+function loadSprites() {
+  loadImage("overworld", "src/zelda-like-tilesets-and-sprites/Overworld.png")
+}
+
+function begin() {
+  bg = document.getElementById("bg") as HTMLCanvasElement;
+  fg = document.getElementById("fg") as HTMLCanvasElement;
+  bg.getContext("2d")?.drawImage(images["overworld"], 0, 0)
+}
+
+function init() {
+  console.log("init", arguments);
+  loadSprites();
+}
+
+document.onreadystatechange = init;

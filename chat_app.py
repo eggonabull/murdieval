6801879285ad887d7c88791266dfd20b9a0c09d1
Drew Faubion <eggonabull@hotmail.com>
2023-03-13 00:40:59 -0700

    Fix a bug in message history. Fix favicon

--- chat_app.py
+++ chat_app.py
@@ -1,11 +1,13 @@
 import os
 
 import openai
-from flask import Flask, redirect, render_template, request, url_for, jsonify
+from flask import Flask, redirect, render_template, request, url_for, jsonify, send_file
 
 static_file_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "static")
 
 openai.api_key_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "openai_api_key.txt")
+favicon_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "static/favicon.ico")
+print("favicon", favicon_path)
 
 app = Flask(
     __name__,
@@ -32,9 +34,13 @@ def chat():
         messages=message_history,
         temperature=0.6,
     )
-    message_history.append({"role": "assistant", "content": response.choices[0]["message"]})
+    message_history.append({"role": "assistant", "content": response.choices[0]["message"]["content"]})
     return jsonify({"text": response})
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     return render_template("index.html")
+
+@app.route("/favicon.ico", methods=["GET"])
+def favicon():
+    return send_file(path_or_file=favicon_path, mimetype="image/ico")

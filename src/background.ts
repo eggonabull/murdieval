2aba2ca32dc5530dd7dd88cff374ca37d7306fc7
Drew Faubion <eggonabull@hotmail.com>
2023-03-23 01:10:32 -0700

    More lint compliance

--- src/background.ts
+++ src/background.ts
@@ -10,7 +10,7 @@ function build_dirt_plot(width: number, height: number): SpriteSourceDef {
   const top = 'dA' + 'dB'.repeat(width - 2) + 'dC';
   const middle = 'eA' + 'eB'.repeat(width - 2) + 'eC';
   const bottom = 'fA' + 'fB'.repeat(width - 2) + 'fC';
-  const map = top + '\n' + ((middle + '\n').repeat(height - 2)) + bottom;
+  const map = top + '\n' + (middle + '\n').repeat(height - 2) + bottom;
   return {
     sprite: 'overworld',
     map: map,
@@ -18,7 +18,7 @@ function build_dirt_plot(width: number, height: number): SpriteSourceDef {
 }
 
 function build_horizontal_bush(length: number): SpriteSourceDef {
-  let map = 'QA' + 'PC'.repeat(length - 2) + 'QB';
+  const map = 'QA' + 'PC'.repeat(length - 2) + 'QB';
   return {
     sprite: 'overworld',
     map: map,
@@ -26,13 +26,13 @@ function build_horizontal_bush(length: number): SpriteSourceDef {
 }
 
 function flatten(layer: Layer): SpriteMapStr {
-  let map = ('  '.repeat(map_width) + '\n')
+  const map = ('  '.repeat(map_width) + '\n')
     .repeat(map_height)
     .slice(0, -1)
     .split('\n')
-    .map((row) => row.split(''));
+    .map(row => row.split(''));
   for (let i = 0; i < layer.length; i++) {
-    let draw_instruction = layer[i];
+    const draw_instruction = layer[i];
     if (!draw_instruction.def.map) {
       /* no collision checking for bounding boxes definitions */
       continue;
@@ -48,32 +48,33 @@ function flatten(layer: Layer): SpriteMapStr {
           .split('')
           .forEach((char, x) => {
             if (char !== ' ') {
-              map[draw_instruction.pos.y + y][draw_instruction.pos.x * 2 + x ] = char;
+              const ypos = draw_instruction.pos.y + y;
+              const xpos = draw_instruction.pos.x * 2 + x;
+              map[ypos][xpos] = char;
             }
           });
       });
   }
-  return map.map((row) => row.join('')).join('\n');
+  return map.map(row => row.join('')).join('\n');
 }
 
-
 function create_collision_map(layer: Layer): SpriteMapStr[] {
-  let result: SpriteMapStr[] = new Array<SpriteMapStr>(map_width * map_height).fill('  ');
+  const result: SpriteMapStr[] = new Array<SpriteMapStr>(map_width * map_height).fill('  ');
   for (let i = 0; i < layer.length; i++) {
-    let def = layer[i].def;
+    const def = layer[i].def;
     if (!def.map) {
       /* no collision checking for bounding boxes definitions */
       continue;
     }
-    let pos = layer[i].pos;
-    let map = def.map.split('\n').map(row => row.trim()).filter(row => row.length > 0);
+    const pos = layer[i].pos;
+    const map = def.map.split('\n').map(row => row.trim()).filter(row => row.length > 0);
     for (let y = 0; y < map.length; y++) {
       for (let x = 0; x < map[0].length; x += 2) {
-        let char = map[y][x] + map[y][x + 1];
+        const char = map[y][x] + map[y][x + 1];
         if (char === ' ' || char === '  ') {
           continue;
         }
-        let index = (pos.y + y) * map_width + (pos.x + x / 2);
+        const index = (pos.y + y) * map_width + (pos.x + x / 2);
         result[index] = char;
       }
     }
@@ -102,7 +103,6 @@ export const up_house_def: SpriteSourceDef = {
   ELEMENEOEP
   `,
 };
-
 
 export const crate: SpriteSourceDef = {
   sprite: 'overworld',
@@ -134,25 +134,31 @@ export const draw_over_player = [
 ];
 
 export const blocks_player = [
-  'FD', 'FE', 'FF', // log
-  'RF', 'RG', // shrub
-  'VV', 'VW', // crate
-  'HD', 'ID', // water
-  'BG', 'BH', 'BI', 'BJ', 'BK', // house
+  // log
+  'FD', 'FE', 'FF', 
+  // shrub
+  'RF', 'RG',
+  // crate
+  'VV', 'VW',
+  // water
+  'HD', 'ID',
+  // house
+  'BG', 'BH', 'BI', 'BJ', 'BK',
   'CG', 'CH', 'CI', 'CJ', 'CK',
   'DG', 'DH', 'DI', 'DJ', 'DK',
   'EG', 'EH', 'EI', 'EJ', 'EK',
-  'BL', 'BM', 'BN', 'BO', 'BP', // up house
+  // up house
+  'BL', 'BM', 'BN', 'BO', 'BP',
   'CL', 'CM', 'CN', 'CO', 'CP',
   'DL', 'DM', 'DN', 'DO', 'DP',
   'EL', 'EM', 'EN', 'EO', 'EP',
 ];
 
 /**
-* Layer0Map is a string that represents the map of the first layer of the game.
-* It is special and defines the height and width of the game and of the
-* collision map.
-*/
+ * Layer0Map is a string that represents the map of the first layer of the game.
+ * It is special and defines the height and width of the game and of the
+ * collision map.
+ */
 export const layer0Sprite = 'overworld';
 const layer0BaseMap = `
 HDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHDHD
@@ -209,18 +215,18 @@ AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
   .filter(row => row.length > 0)
   .join('\n');
 export const layer0: Layer = [
-  {def: { sprite: layer0Sprite, map: layer0BaseMap}, pos: {x: 0, y: 0}},
+  {def: {sprite: layer0Sprite, map: layer0BaseMap}, pos: {x: 0, y: 0}},
   {def: build_dirt_plot(7, 7), pos: {x: 40, y: 14}},
 ];
-let map_height = layer0BaseMap.split('\n').length;
-let map_width = layer0BaseMap.split('\n')[0].length / 2;
+const map_height = layer0BaseMap.split('\n').length;
+const map_width = layer0BaseMap.split('\n')[0].length / 2;
 console.log(
   'map_width: ',
   map_width,
   'map_height: ',
   map_height,
   'layer0Map',
-  '\'' + layer0BaseMap + '\''
+  "'" + layer0BaseMap + "'"
 );
 export const layer0Map = flatten(layer0);
 
